31a4d1f9c96a5d8197712fa1524c17d49d21e680
spidunno <70819261+spidunno@users.noreply.github.com>
2024-07-04 12:11:29 -0400

    see last commit

--- src/util.ts
+++
@@ -1,3 +0,0 @@
-export const TEST_CONSTANT = "Remove this when you add your own module files that you import from main.ts. This is an export, \
-it wouldn't be here, but TypescriptToLua has a bug where if you only have a single file with no imports or exports, everything runs twice. \
-This export will be removed, along with this file, in a future update as soon as the TypescriptToLua team fixes it.";
