d39df529b1e5f68a9c055890be0fa64d64b52b35
spidunno <70819261+spidunno@users.noreply.github.com>
2024-07-08 20:36:55 +0000

    changed to tmplr

--- main.lua
+++ main.lua
@@ -8,8 +8,10 @@ local function require(file, ...)
     end
     if ____modules[file] then
         local module = ____modules[file]
-        ____moduleCache[file] = { value = (select("#", ...) > 0) and module(...) or module(file) }
-        return ____moduleCache[file].value
+        local value = nil
+        if (select("#", ...) > 0) then value = module(...) else value = module(file) end
+        ____moduleCache[file] = { value = value }
+        return value
     else
         if ____originalRequire then
             return ____originalRequire(file)
@@ -19,18 +21,11 @@ local function require(file, ...)
     end
 end
 ____modules = {
-["src.util"] = function(...) 
-local ____exports = {}
-____exports.TEST_CONSTANT = "Remove this when you add your own module files that you import from main.ts. This is an export, it wouldn't be here, but TypescriptToLua has a bug where if you only have a single file with no imports or exports, everything runs twice. Will be removed, along with this file, in a future update as soon as the TypescriptToLua team fixes it."
-return ____exports
- end,
 ["src.main"] = function(...) 
-local ____exports = {}
 _G.update = function()
     tm.os:Log("Hello, world!")
 end
 local ____ = tm
-return ____exports
  end,
 }
 return require("src.main", ...)
